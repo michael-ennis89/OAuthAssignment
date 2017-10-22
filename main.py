a49b975000dad84c4abb21854cad57ae647c11b6
michael-ennis89 <ennism@oregonstate.edu>
2017-10-21 20:34:52 -0700

    updates

--- main.py
+++ main.py
@@ -11,19 +11,34 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+	
+class State(ndb.Model):
+	id = ndb.StringProperty(),
+	state = ndb.StringProperty()
 
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.write('Hello')
+		state = hashlib.sha256(os.urandom(1024)).hexdigest()
+		template_values = {
+			'state' : state
+		}
+		
+		new_key = State(id="", state=state)
+		new_key.put()
+		new_key.id = str(new_key.key.id())
+		new_key.put()
+		
 		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.write(template.render())
+		self.response.write(template.render(template_values))
 		
 class OauthHandler(webapp2.RequestHandler):
 	def get(self):
+		self.response.write('Oauth')
 	
 class DisplayHandler(webapp2.RequestHandler):
 	def post(self):
+		self.response.write('DisplayHandler')
 
 
 # [START app]
